d62f70ecb28b69473f8659f4c4b32849fb7ef136
Michael Munoz <mmunoz779@outlook.com>
2019-02-23 09:34:00 -0500

    removed unused framework code in CSS

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.get('/', function (req, res, next) {
+    if (!req.session) {
+        fs
+    }
     res.render('home.html');
 });
 
